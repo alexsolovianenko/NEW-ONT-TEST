3ccd0944ced11ce3d1dc0baa3ab2d0d935a981dd
alexsolovianenko <solovianenkobusiness@gmail.com>
2025-04-09 20:38:52 -0400

    Add code for file processing and OpenAI integration

--- Second-Step/main.py
+++ Second-Step/main.py
@@ -1,6 +1,10 @@
 #Step 1: start | use python main.py
 
 
+
+
+
+
 # Step 2: import libraries
 import boto3
 import openai
@@ -8,6 +12,10 @@ import os
 from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 from reportlab.pdfgen import canvas
+
+
+
+
 
 
 # Step 3: import aws credentials
@@ -21,7 +29,7 @@ second_aws_secret_access_key = os.getenv('SECOND_AWS_SECRET_ACCESS_KEY')
 second_aws_region = os.getenv('SECOND_AWS_REGION')
 second_s3_bucket_name = os.getenv('SECOND_S3_BUCKET_NAME')
 openai_api_key = os.getenv('OPENAI_API_KEY')
-
+openai.api_key = openai_api_key
 
 # start s3 client
 s3_client = boto3.client(
@@ -29,6 +37,12 @@ s3_client = boto3.client(
     aws_access_key_id=first_aws_access_key_id,
     aws_secret_access_key=first_aws_secret_access_key
 )
+
+
+
+
+
+
 
 
 
@@ -44,7 +58,11 @@ if 'Contents' in response:
 
     print(f"Fetching the latest file: {object_key}")
 
-    # Step 6: Download the file locally
+
+
+
+
+# Step 5: Download the file
     file_response = s3_client.get_object(Bucket=first_s3_bucket_name, Key=object_key)
     file_content = file_response['Body'].read()
 
@@ -56,30 +74,46 @@ if 'Contents' in response:
 else:
     print("No files found in the S3 bucket.")
 
-# Step 5: Read the PDF file
+
+
+
+
+# Step 6: Read the PDF file
 def read_pdf(file_path):
     reader = PdfReader(file_path)
     text = ''
     for page in reader.pages:
         text += page.extract_text()
     return text
-
-# Step 6: Read the PDF file
 pdf_text = read_pdf(object_key)
 
 print("PDF file read successfully.")
 
 
-# Step 7: Set up OpenAI API key
-openai.api_key = openai_api_key
-# Step 8: Generate a summary using OpenAI API
-def generate_summary(text):
+
+
+
+# Step 7: Generate new questions using OpenAI API
+
+
+
+def generate_questions(text):
+    prompt = f"Based on the following text, generate 2 questions with using the same subject(course) information:\n\n{text}"
+    
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": f"Please summarize the following text:\n\n{text}"}
+            {"role": "user", "content": prompt}
         ]
     )
+    print("Questions generated successfully.")
     return response['choices'][0]['message']['content']
+    
 
 
+
+# Step 8: Create a new PDF using labpdf
+
+# Step 9: Paste the questions in the new PDF
+
+# Step 10: Upload the new PDF to the second S3 bucket

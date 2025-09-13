e42ed2366c243855c7c727ccdae4b7a448825c63
alexsolovianenko <solovianenkobusiness@gmail.com>
2025-09-13 10:14:28 -0400

    dev branch

--- .Main-Server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const fileRoutes = require('../dThird-Step/Backend/server'); // Adjust the path as needed
-const app = express();
-const port = 8000;
-app.use('/api', fileRoutes);
-app.listen(port, () => {
-    console.log(`Main server is running on http://localhost:${port}`);
-});
